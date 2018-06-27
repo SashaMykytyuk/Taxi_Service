1a2159a8d0c27170b4e52f7139e52dfcc4287759
Rengan Elizabeth <miss.elizaveta.rengan@gmail.com>
2018-06-27 16:34:39 +0300

    Added DAL and Model

---
+++ DAL/DAL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Як правильно розбить ці ф-ції у DAL i IDAL ?
+namespace DAL
+{
+    public interface IDAL
+    {
+        void Create<T>(T _element) where T: class;
+        void Delete<T>(T _element) where T: class;
+        ICollection<T> Get<T>() where T : class;
+        void Update<T>(T item) where T : class;
+    }
+
+
+    public class DAL : IDAL
+    {
+        DbContext context;
+        public DAL(DbContext _context)
+        {
+            context = _context;
+        }
+
+        public void Create<T>(T _element) where T : class
+        {
+            context.Set<T>().Add(_element);
+            context.SaveChanges();
+        }
+
+        public void Delete<T>(T _element) where T : class
+        {
+            context.Set<T>().Remove(_element);
+            context.SaveChanges();
+        }
+
+        public ICollection<T> Get<T>() where T : class
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void Update<T>(T item) where T:class
+        {
+            context.Entry(item).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DAL/Model.cs
@@ -0,0 +1,23 @@
+namespace DAL
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class TaxiContext : DbContext
+    {
+     
+        public TaxiContext()
+            : base("name=Model")
+        {
+        }
+
+        public virtual DbSet<Location> Locations { get; set; }
+        public virtual DbSet<Client> Clients { get; set; }
+        public virtual DbSet<Driver> Drivers { get; set; }
+        public virtual DbSet<Dispatcher> Dispatchers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<Report> Reports { get; set; }
+    }
+}
