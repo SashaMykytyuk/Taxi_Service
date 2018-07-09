6984efefb7b440f5568f3f2bebb281954d50ace9
Sasha.Mykytyuk86 <Sasha.Mykytyuk86@gmail.com>
2018-07-09 17:09:40 +0300

    V1

--- BingMapUI/BingMapUI/MainWindow.xaml.cs
+++ BingMapUI/BingMapUI/MainWindow.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Maps.MapControl.WPF;
-using System.Windows.Media.Imaging;
 
 
 namespace BingMapUI
